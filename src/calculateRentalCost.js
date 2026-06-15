e3912ea7d7675fe873f2e9e21f1be9a948ffc843
Anik <anikferreira8@gmail.com>
2026-06-15 14:33:52 -0300

    finish calculateRentalCost.js

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,14 @@
-/**
- * @param {number} days
- *
- * @return {number}
- */
 function calculateRentalCost(days) {
-  // write code here
+  const dailyCost = 40;
+  let total = days * dailyCost;
+
+  if (days >= 7) {
+    total -= 50;
+  } else if (days >= 3) {
+    total -= 20;
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
